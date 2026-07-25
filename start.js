5ea1fc0b0d12e5a3fe27ca05f6cf502ade670ece
Darryl Tan <204873794+darryl-land@users.noreply.github.com>
2026-07-25 21:46:01 +0800

    Update start.js
    Added yt-dlp video downloader function and updater

--- start.js
+++ start.js
@@ -1,4 +1,3 @@
-
 global.timert = {
     msRemaining: 0,
     targetTime: null,
@@ -32,4 +31,45 @@ global.stopCountdown = function() {
     global.timert.msRemaining = 0;
 };
 
+
+const { exec } = require('child_process');
+
+
+
 nw.Window.open('index.html', {}, function(win) {});
+
+
+global.downloadyt = function(urlthing, sblockk, subb) {
+    console.log(urlthing + sblockk)
+    let cmdd = './yt-dlp_linux -P "~/Videos" ';
+    if (sblockk) {
+        cmdd += '--sponsorblock-remove default ';
+    };
+    if (subb){
+        cmdd += '--write-auto-subs --sub-lang en --embed-subs ';
+    };
+    cmdd += urlthing
+    console.log(cmdd)
+    exec(cmdd, { shell: '/bin/bash' },(error, stdout, stderr) => {
+        if (error) {
+            console.error(error.message);
+            return;
+        }
+        if (stderr) {
+            console.warn(stderr);
+            return;
+        }
+        console.log(stdout);
+    });
+}
+
+exec('./yt-dlp_linux -U', { shell: '/bin/bash' }, (err, stdout, stderr) => {
+    if (err) {
+        console.error(`Error: ${err}`);
+        return;
+    }
+    console.log(`Output: ${stdout}`);
+});
+
+
+
